d76cabed12c8e800ddfd6a08546c4e8139bf86d9
DeltaDizzy <EJBraswell@gmail.com>
2020-02-23 22:10:33 -0600

    Create ImageEffect.cs

---
+++ ImageEffects/ImageEffect.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KSP-API
+{
+  public class ImageEffect : MonoBehaviour
+  {
+    [SerializeField]
+    protected Shader shader;
+    protected Material mat;
+    
+    private void Awake()
+    {
+      mat = new Material(shader);
+    }
+    
+    protected virtual void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+      Graphics.Blit(src, dst, mat);
+    }
+  }
+}
