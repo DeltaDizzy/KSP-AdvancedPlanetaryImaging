b22aa524db81375237559111626e97e4050b1b1a
DeltaDizzy <ejbraswell@gmail.com>
2020-02-29 00:22:12 -0600

    VS Project

---
+++ API_Plugin/src/AdvancedPlanetaryImaging/APISettings.cs
@@ -0,0 +1,21 @@
+﻿using Kopernicus.ConfigParser.Attributes;
+using Kopernicus.ConfigParser.Enumerations;
+using Kopernicus.ConfigParser.BuiltinTypeParsers;
+using System;
+using System.Collections.Generic;
+
+namespace AdvancedPlanetaryImaging
+{
+    [RequireConfigType(ConfigType.Node)]
+    public class APISettings
+    {
+        private Boolean debug = false;
+
+        [ParserTarget("debugMode")]
+        public NumericParser<Boolean> Debug = new NumericParser<bool>(false);
+
+        
+        [ParserTargetCollection("Overlays", AllowMerge = true)]
+        List<String> Overlays { get; set; }
+    }
+}
